cfc55406f31a00db44ad710e4e7aa8ff1f107de8
raamy24a <radib@shi-r2-p5.s19.be>
2026-03-09 16:49:16 +0100

    a

--- src/raycast.c
+++ src/raycast.c
@@ -6,7 +6,7 @@
 /*   By: radib <radib@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 14:35:49 by radib             #+#    #+#             */
-/*   Updated: 2026/03/09 14:49:22 by radib            ###   ########.fr       */
+/*   Updated: 2026/03/09 16:46:14 by radib            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 float	angle_calc(float angle, float calc)
 {
-	angle = angle + calc - 45;
+	angle = angle + calc - 33;
 	if (angle < 0)
 		angle = 360 + angle;
 	else if (angle > 360)
@@ -31,7 +31,9 @@ int	**raycast(t_c **c, int i, float angles)
 
 	while (i < p->width)
 	{
-		
+		p->pos_x;
+		p->pos_y;
+		angles = angle_calc(p->angle, 66 / p->width * i);
 		p->rays[0] = ;
 		p->rays[1] = ;
 	}
